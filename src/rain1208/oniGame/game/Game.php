1ce8e625e567dc18bc83fa94ba187f4cbfe51449
raink1208 <58714901+raink1208@users.noreply.github.com>
2020-07-10 04:08:21 +0900

    create Files

--- src/rain1208/oniGame/game/Game.php
+++ src/rain1208/oniGame/game/Game.php
@@ -25,16 +25,33 @@ class Game
     public function setMap(Map $map):void
     {
         $this->map = $map;
-
     }
 
     public function startGame():void
     {
         OniGame::getInstance()->getScheduler()->scheduleRepeatingTask($this->gameTask = new GameTask($this),20);
+        OniGame::getInstance()->getServer()->broadcastMessage("ゲームを開始します");
+    }
+
+    public function spectate(Player $player): void
+    {
+        $this->players[$player->getName()]->setSpectating();
+        $player->setGamemode(Player::SPECTATOR);
     }
 
     public function joinGame(Player $player): void
     {
         $this->players[$player->getName()] = new GamePlayer($player);
+    }
+
+    public function leaveGame(Player $player):void
+    {
+        unset($this->players[$player->getName()]);
+    }
+
+    public function endGame(): void
+    {
+        OniGame::getInstance()->getServer()->broadcastMessage("ゲームを終了します");
+        OniGame::getInstance()->getScheduler()->cancelTask($this->gameTask->getTaskId());
     }
 }
