36a48ac732b40a2bc9c6053eb3dd2548762ca27f
raink1208 <58714901+raink1208@users.noreply.github.com>
2020-07-12 21:21:35 +0900

    Add commandRegistar

--- src/rain1208/oniGame/OniGame.php
+++ src/rain1208/oniGame/OniGame.php
@@ -5,7 +5,9 @@ namespace rain1208\oniGame;
 
 
 use pocketmine\plugin\PluginBase;
+use rain1208\oniGame\event\PlayerEventListener;
 use rain1208\oniGame\game\Game;
+use rain1208\oniGame\map\MapManager;
 
 class OniGame extends PluginBase
 {
@@ -15,6 +17,7 @@ class OniGame extends PluginBase
     private static $instance;
 
     private $configManager;
+    private $mapManager;
 
     /** @var Game */
     private $game;
@@ -22,6 +25,27 @@ class OniGame extends PluginBase
     public function onEnable()
     {
         self::$instance = $this;
+        $this->getServer()->getPluginManager()->registerEvents(new PlayerEventListener(), $this);
+
+        $this->configManager = new ConfigManager();
+        $this->mapManager = new MapManager();
+
+        $this->registarCommand();
+    }
+
+    private function registarCommand(): void {
+        $map = $this->getServer()->getCommandMap();
+        $command = [
+            "setOni" => "rain1208\onigame\command\SetOni",
+            "startOni" => "rain1208\onigame\command\StartOni",
+            "setMap" => "rain1208\onigame\command\SetMap",
+            "joinoni" => "rain1208\onigame\command\JoinGame",
+            "createoni" => "rain1208\onigame\command\CreateGame"
+        ];
+
+        foreach ($command as $item => $class) {
+            $map->register("onigame", new $class($this));
+        }
     }
 
     public static function getInstance():OniGame
@@ -38,6 +62,11 @@ class OniGame extends PluginBase
     {
         $this->game = new Game();
         $this->getServer()->broadcastMessage(self::PLGUIN_TAG."ゲームを生成しました");
+    }
+
+    public function issetGame(): bool
+    {
+        return (isset($game))? true : false;
     }
 
     public function startGame(): void
